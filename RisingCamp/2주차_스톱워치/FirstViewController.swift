09a30e2f5a38a9dfe5efcb6d4ecc96cdd04ad71b
LeeHa-Yeon <gkdus1613@naver.com>
2021-10-27 19:03:46 +0900

    알라미 프론트 구성

---
+++ RisingCamp/2주차_스톱워치/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  RisingCamp
+//
+//  Created by 이하연 on 2021/10/27.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
