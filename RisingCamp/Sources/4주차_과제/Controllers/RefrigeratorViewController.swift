4d5957d93dcce9d1d1ac05a4da0e48b934801561
LeeHa-Yeon <gkdus1613@naver.com>
2021-11-17 19:24:46 +0900

    화면 UI 설계

---
+++ RisingCamp/Sources/4주차_과제/Controllers/RefrigeratorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RefrigeratorViewController.swift
+//  RisingCamp
+//
+//  Created by 이하연 on 2021/11/17.
+//
+
+import UIKit
+
+class RefrigeratorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
